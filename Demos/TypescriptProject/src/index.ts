30f8c79e0bb0b28f600252cd2d9ae6672836779f
DEV <DEV@DEV.COM>
2025-05-27 14:06:01 +0530

    commit

--- Demos/TypescriptProject/src/index.ts
+++ Demos/TypescriptProject/src/index.ts
@@ -1,1 +1,10 @@
 console.log("Welcome to the TypeScript Project!");
+
+// Prints a random number between 0 (inclusive) and 1 (exclusive)
+function printRandomNumber() {
+    const randomNum = Math.random();
+    console.log("Random number:", randomNum);
+}
+
+printRandomNumber();
+
